0b2e548a9140b089d6b67688ad4f18bf0b5424b5
Emre Şekeroğlu <emresekeroglu12@gmail.com>
2022-10-31 15:39:46 +0100

    necessary functions are working properly

--- lib/screens/france_screen.dart
+++ lib/screens/france_screen.dart
@@ -1,2 +1,51 @@
+import 'package:calendar/services/calendar.dart';
 import 'package:flutter/material.dart';
-import 'package:calendar/utilities/constants.dart';
+
+class FranceScreen extends StatefulWidget {
+  const FranceScreen({this.franceCalendar});
+  final franceCalendar;
+
+  @override
+  State<FranceScreen> createState() => _FranceScreenState();
+}
+
+class _FranceScreenState extends State<FranceScreen> {
+  CalendarModel calendar = CalendarModel();
+  String holidayName = '';
+  int year = 0;
+  int month = 0;
+  int day = 0;
+
+  void updateUI(dynamic calendarData) {
+    setState(() {
+      if (calendarData == null) {
+        holidayName = 'Cannot get holiday information';
+        year = 0;
+        month = 0;
+        day = 0;
+        return;
+      }
+      holidayName = calendarData['response']['holidays'][0]['name'];
+      year =
+          calendarData['response']['holidays'][0]['date']['datetime']['year'];
+      month =
+          calendarData['response']['holidays'][0]['date']['datetime']['month'];
+      day = calendarData['response']['holidays'][0]['date']['datetime']['day'];
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updateUI(widget.franceCalendar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:
+          Text('holiday name: $holidayName year: $year month $month day: $day'),
+    );
+  }
+}
