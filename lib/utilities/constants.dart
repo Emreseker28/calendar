192dcec6fd344fb3ad34e1aef0a470ca379781b6
Emre Şekeroğlu <emresekeroglu12@gmail.com>
2022-10-31 14:45:33 +0100

    changed URL constant

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const calendarificURL = "https://calendarific.com/api/v2";
+const calendarificURL = "https://calendarific.com/api/v2/holidays";
 const apiKey = "33f2a05808e7f8fe6a04a289e1a7c8cb60564b48";
 const franceISO = "FR";
 const spainISO = "ES";
