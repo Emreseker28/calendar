0edf6186ee58b2795f8deb3d8e111ffbe1f54332
Emre Şekeroğlu <emresekeroglu12@gmail.com>
2022-10-31 12:21:33 +0100

    init commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const calendarificURL = "https://calendarific.com/api/v2";
+const apiKey = "33f2a05808e7f8fe6a04a289e1a7c8cb60564b48";
+const franceISO = "FR";
+const spainISO = "ES";
+
+//request: curl 'https://calendarific.com/api/v2/holidays?&api_key=API_KEY&country=&year='
