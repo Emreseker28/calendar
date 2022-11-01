648bd50398dc7ff8d973e7cc75d826fdd74dfbd0
Emre Şekeroğlu <emresekeroglu12@gmail.com>
2022-11-01 15:59:29 +0100

    added ListView

--- lib/screens/france_screen.dart
+++ lib/screens/france_screen.dart
@@ -1,4 +1,5 @@
 import 'package:calendar/services/calendar.dart';
+import 'package:calendar/utilities/constants.dart';
 import 'package:flutter/material.dart';
 
 class FranceScreen extends StatefulWidget {
@@ -15,6 +16,10 @@ class _FranceScreenState extends State<FranceScreen> {
   int year = 0;
   int month = 0;
   int day = 0;
+  var holidayList = [];
+  var yearList = [];
+  var monthList = [];
+  var dayList = [];
 
   void updateUI(dynamic calendarData) {
     setState(() {
@@ -25,27 +30,53 @@ class _FranceScreenState extends State<FranceScreen> {
         day = 0;
         return;
       }
-      holidayName = calendarData['response']['holidays'][0]['name'];
-      year =
-          calendarData['response']['holidays'][0]['date']['datetime']['year'];
-      month =
-          calendarData['response']['holidays'][0]['date']['datetime']['month'];
-      day = calendarData['response']['holidays'][0]['date']['datetime']['day'];
+
+      for (int i = 0; i < 24; i++) {
+        var holiday = widget.franceCalendar['response']['holidays'][i]['name'];
+        var yearToAdd =
+            calendarData['response']['holidays'][i]['date']['datetime']['year'];
+        var monthToAdd = calendarData['response']['holidays'][i]['date']
+            ['datetime']['month'];
+        var dayToAdd =
+            calendarData['response']['holidays'][i]['date']['datetime']['day'];
+        holidayList.add(holiday);
+        yearList.add(yearToAdd);
+        monthList.add(monthToAdd);
+        dayList.add(dayToAdd);
+      }
+      holidayName = holidayList[0];
+      year = yearList[0];
+      month = monthList[0];
+      day = dayList[0];
     });
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     updateUI(widget.franceCalendar);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:
-          Text('holiday name: $holidayName year: $year month $month day: $day'),
-    );
+    return ListView(children: [
+      Column(
+        children: List.generate(holidayList.length, (index) {
+          return Container(
+            child: Column(
+              children: [
+                Text(
+                  'Holiday: ${holidayList[index]}, Date: ${dayList[index]}-${monthList[index]}-${yearList[index]}',
+                  style: kMessageTextStyle,
+                ),
+                SizedBox(
+                  height: 20.0,
+                )
+              ],
+            ),
+          );
+        }),
+      ),
+    ]);
   }
 }
