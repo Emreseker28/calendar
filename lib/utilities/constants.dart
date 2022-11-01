ace297336fe4514a7dc5843a31b155ce3e26cd0b
Emre Şekeroğlu <emresekeroglu12@gmail.com>
2022-11-01 16:00:32 +0100

    added LoadingSpin, text decoration

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const calendarificURL = "https://calendarific.com/api/v2/holidays";
 const apiKey = "33f2a05808e7f8fe6a04a289e1a7c8cb60564b48";
@@ -8,13 +9,31 @@ const spainISO = "ES";
 //request: curl 'https://calendarific.com/api/v2/holidays?&api_key=API_KEY&country=&year='
 
 const kMessageTextStyle = TextStyle(
-  fontFamily: 'Spartan MB',
-  fontSize: 20.0,
-  color: Colors.white,
-);
+    fontFamily: 'Spartan MB',
+    fontSize: 20.0,
+    color: Colors.white,
+    decoration: TextDecoration.none);
 
 const kButtonTextStyle = TextStyle(
-  fontSize: 30.0,
-  fontFamily: 'Spartan MB',
-  color: Colors.white,
+    fontSize: 30.0,
+    fontFamily: 'Spartan MB',
+    color: Colors.white,
+    decoration: TextDecoration.none);
+
+const kLoadingSpin = Center(
+  child: SpinKitDoubleBounce(
+    color: Colors.white,
+    size: 100.0,
+  ),
 );
+
+ListView kTextBuilder = ListView.builder(
+    padding: const EdgeInsets.all(8),
+    itemBuilder: (BuildContext context, int index) {
+      return ListTile(
+        trailing: Text(
+          'Holiday: ',
+          style: kMessageTextStyle,
+        ),
+      );
+    });
